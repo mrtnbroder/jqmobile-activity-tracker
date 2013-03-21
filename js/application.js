dabb1a1e6c0969385e6bebbb73a5071fe0a6296e
MetalMatze <git-desktop@metalmatze.de>
2013-03-22 00:19:51 +0100

    application.js - move displayCounter() out of pageinit for testing

--- js/application.js
+++ js/application.js
@@ -7,21 +7,20 @@ function parseTime($num)
 	return $num;
 }
 
-	(function(){
-		var activity_now = new Date();
-		var activity_time = Math.abs(activity_now - activity_start);
+function displayCounter(activity_start)
+{
+	var activity_now = new Date();
+	var activity_time = Math.abs(activity_now - activity_start);
 
-		activity_hours = parseTime(parseInt(activity_time/(1000*60*60)));
-		activity_minutes = parseTime(parseInt(activity_time/(1000*60))%60);
-		activity_seconds = parseTime(parseInt(activity_time/1000)%60);
-		activity_milliseconds = parseTime(parseInt(activity_time/10)%60);
+	activity_hours = parseTime(parseInt(activity_time/(1000*60*60)));
+	activity_minutes = parseTime(parseInt(activity_time/(1000*60))%60);
+	activity_seconds = parseTime(parseInt(activity_time/1000)%60);
+	activity_milliseconds = parseTime(parseInt(activity_time/10)%60);
 
-		$('.activity_counter.js .activity_hours').text(activity_hours);
-		$('.activity_counter.js .activity_minutes').text(activity_minutes);
-		$('.activity_counter.js .activity_seconds').text(activity_seconds);
-		$('.activity_counter.js .activity_milliseconds span').text(activity_milliseconds);
-		// console.log(activity_hours + ' ' + activity_minutes + ' ' + activity_seconds + ' ' + activity_milliseconds);
-		setTimeout(arguments.callee, 10);
-	})();
-
-});
+	$('.activity_counter.js .activity_hours').text(activity_hours);
+	$('.activity_counter.js .activity_minutes').text(activity_minutes);
+	$('.activity_counter.js .activity_seconds').text(activity_seconds);
+	$('.activity_counter.js .activity_milliseconds span').text(activity_milliseconds);
+	console.log(activity_hours + ' ' + activity_minutes + ' ' + activity_seconds + ' ' + activity_milliseconds);
+	setTimeout(function() {displayCounter(activity_start);}, 10);
+}

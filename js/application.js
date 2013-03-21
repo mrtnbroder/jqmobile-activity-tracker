826d5ad9793db764e250c47c0c96b7ebc55a874d
MetalMatze <git-desktop@metalmatze.de>
2013-03-21 01:42:16 +0100

    time counter created

--- js/application.js
+++ js/application.js
@@ -3,5 +3,30 @@
 	application.js
 */
 $(document).on("pageinit", function(event){
-	// custom code goes here
+
+	var activity_start = new Date() - 4980000;  // some date
+
+	function parseTime($num)
+	{
+		if($num < 10) return '0' + $num;
+		return $num;
+	}
+
+	(function(){
+		var activity_now = new Date();
+		var activity_time = Math.abs(activity_now - activity_start);
+
+		activity_hours = parseTime(parseInt(activity_time/(1000*60*60))%24);
+		activity_minutes = parseTime(parseInt(activity_time/(1000*60))%60);
+		activity_seconds = parseTime(parseInt(activity_time/1000)%60);
+		activity_milliseconds = parseTime(parseInt(activity_time/10)%60);
+
+		$('.activity_hours').text(activity_hours);
+		$('.activity_minutes').text(activity_minutes);
+		$('.activity_seconds').text(activity_seconds);
+		$('.activity_milliseconds span').text(activity_milliseconds);
+		// console.log(activity_hours + ' ' + activity_minutes + ' ' + activity_seconds + ' ' + activity_milliseconds);
+		setTimeout(arguments.callee, 10);
+	})();
+
 });
